94f9e58ec2bb920cc025b2beaeede64db258cd58
Joshua1491 <your.email@example.com>
2025-07-04 02:05:37 +0530

    feat(dashboard): apply Aurachat gradients & icons

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,16 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default withBundleAnalyzer(nextConfig);
