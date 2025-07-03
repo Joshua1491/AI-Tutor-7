acffd443e9fe5fdc3f5d476c1e0468e97d8e20e7
Joshua1491 <your.email@example.com>
2025-07-03 21:49:19 +0530

    chore: fix ESLint require rule and update Husky pre-commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore – @next/bundle-analyzer has no type definitions
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
